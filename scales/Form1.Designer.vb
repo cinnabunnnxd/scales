997841767666388b12391ecab12a5b8660dc3427
rohyoonseo <109777617+cinnabunnnxd@users.noreply.github.com>
2024-08-19 18:42:38 +0800

    Add files via upload

---
+++ scales/Form1.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(173, 146)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(128, 200)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(202, 118)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(101, 56)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 23)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(107, 361)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 15)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Label1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ scales/Form1.vb
@@ -0,0 +1,36 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Scale As Double
+
+        Scale = TextBox1.Text
+
+
+        If Scale >= 1.0 And Scale <= 2.9 Then
+            Label1.Text = "Micro - not felt, but can be recorded by topography"
+            PictureBox1.Image = My.Resources.Screenshot_2023_03_02_111200
+        ElseIf Scale >= 3.0 And Scale <= 3.9 Then
+            Label1.Text = "Minor - Vibrations detected "
+            PictureBox1.Image = My.Resources.Screenshot_2023_03_02_111208
+        ElseIf Scale >= 4.0 And Scale <= 4.9 Then
+            Label1.Text = "Light - Vibrations detected"
+            PictureBox1.Image = My.Resources.Screenshot_2023_03_02_111216
+        ElseIf Scale = 5.0 And Scale <= 5.9 Then
+            Label1.Text = "Moderate - Window rattle or break, light damage"
+            PictureBox1.Image = My.Resources.Screenshot_2023_03_02_111223
+        ElseIf Scale = 6.0 And Scale <= 6.9 Then
+            Label1.Text = "Strong - Cracks in buildings, falling branches"
+            PictureBox1.Image = My.Resources.Screenshot_2023_03_02_111236
+        ElseIf Scale >= 7.0 And Scale <= 7.9 Then
+            Label1.Text = "Major - building collapses, landslides"
+            PictureBox1.Image = My.Resources.Screenshot_2023_03_02_111251
+        ElseIf Scale > 8.0 Then
+            Label1.Text = "Great - Devastation, many deaths"
+            PictureBox1.Image = My.Resources.Screenshot_2023_03_02_111301
+
+        End If
+    End Sub
+End Class
